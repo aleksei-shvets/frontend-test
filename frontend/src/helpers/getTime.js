bf0ff015c698f6e016d1eac0e50e6afa09828721
aleksei-shvetz <al.sv.dev@gmail.com>
2024-06-22 01:43:11 +0300

    Fix getTime

--- frontend/src/helpers/getTime.js
+++ frontend/src/helpers/getTime.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 const getTime = (timeStr) => {
   const [, time] = timeStr.trim().split('T');
-  const [hours, minutes, ...rest] = time;
+  const [hours, minutes, ...rest] = time.split(':');
   return `${hours}:${minutes}`;
 };
 
